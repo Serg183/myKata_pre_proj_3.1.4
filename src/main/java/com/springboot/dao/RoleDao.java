45baa158357d16fc021483b997f1cc37092c4947
Serg183 <sergmygit@gmail.com>
2022-08-04 21:04:30 +1000

    commit2

---
+++ src/main/java/com/springboot/dao/RoleDao.java
@@ -0,0 +1,15 @@
+package com.springboot.dao;
+
+import com.springboot.model.Role;
+import java.util.List;
+
+public interface RoleDao {
+
+    List<Role> getAllRoles();
+
+    Role getRole(String userRole);
+
+    Role getRoleById(Long id);
+
+    void addRole(Role role);
+}
