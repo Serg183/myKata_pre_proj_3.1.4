788039e67b1e7954ced3a8c08794a1379a560c3f
Serg183 <sergmygit@gmail.com>
2022-08-06 01:19:43 +1000

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/Exception/UserUsernameExistException.java
@@ -0,0 +1,9 @@
+package com.springbootrest.Exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class UserUsernameExistException extends DataIntegrityViolationException {
+    public UserUsernameExistException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -0,0 +1,12 @@
+package com.springbootrest.service;
+
+import com.springbootrest.model.Role;
+
+import java.util.List;
+import java.util.Set;
+
+public interface RoleService {
+    List<Role> findAllRole();
+    void addDefaultRole();
+    Set<Role> findByIdRoles(List<Long>roles);
+}
